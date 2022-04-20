0c15099afccc2bad49bdc9aa9cfb836459e24ace
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-21 01:53:22 +0400

    make validatin in type

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,30 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import "./input.css"
 
-export default function InputComp({require, value, type, name, placeholder, message, onChange }) {
-    const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
-    const passwordValidation = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/i
-    const [error,setError]= useState("")
-
-    const handleKeyDown = (e) =>{
-        if(e.target.value.length < 3 ){
-            setError("Min length is 3")
-        }  else if(name === "email" && !emailValidation.test(e.target.value)){
-            setError("Please enter valid email")
-        } else if(name === "password" && !passwordValidation.test(e.target.value)){
-            setError("Min 8 characters, 1 letter, 1 number and 1 special character")
-        } else {
-            setError("")
-        }
-    }
-
+export default function InputComp({className, type, name, placeholder, onChange, error }) {
     return (
         <div className="inputComp">
-            <div className="input" style={{ border: require && "1px solid red" }}>
-                <input value={value} type={type} name={name} placeholder={placeholder} autoComplete="off" onChange={onChange} onKeyDown={handleKeyDown}/>
-                <div>{require && <span>x</span>}</div>
+            <div className={`input ${className}`}>
+                <input type={type} name={name} placeholder={placeholder} autoComplete="off" onChange={onChange}/>
+                <div>{className==="invalid" && <span>x</span>}</div>
             </div>
-            <span>{message || error}</span>
+            <span>{error}</span>
         </div>
     );
 }
--- src/components/register/index.js
+++ src/components/register/index.js
@@ -4,38 +4,90 @@ import InputComp from '../Input';
 import "./register.css"
 
 export default function Register() {
-  const [formData, setFormData] = useState({})
-  const [errorMessage, setErrorMessage] = useState({})
-  const [require, setRequire] = useState(false)
+  const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i;
+  const passwordValidation = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/i
   const countryPhoneCode={
     "Select":"Select",
     "Afghanistan": "+93",
     "Albania": "+355",
     "Armenia": "+374",
   }
-
+  const [formData, setFormData] = useState({
+    name: "",
+    selectCountry:"",
+    selectCode: "",
+    tel:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+    agreeCheckbox:""
+  })
+  const [className, setClassName]  = useState({})
+  const [errors,setErrors]= useState({})
+ 
   const handleChange = (e) => {
+    if(e.target.value.length < 3 ){
+      setErrors({
+        ...errors,
+        [e.target.name] : "Min length is 3"
+      })
+    }  else if(e.target.name === "email" && !emailValidation.test(e.target.value)){
+        setErrors({
+          ...errors,
+          [e.target.name] : "Please enter valid email"
+        })
+    } else if(e.target.name === "password" && !passwordValidation.test(e.target.value)){
+        setErrors({
+          ...errors,
+          [e.target.name] :  "Min 8 characters, 1 letter, 1 number and 1 special character"
+        })
+    } else {
+        setErrors("")
+    }
     setFormData({
       ...formData,
       [e.target.name]: e.target.value.trim()
     });
-    setRequire(false)
-  };
+    setClassName(elemClass => {
+      return {
+         ...elemClass,
+         [e.target.name]: 'default',
+      }
+   })
+   Object.keys(formData).forEach(x=>{
+    if(formData[x] && !errors[x]){
+      setClassName(elemClass => {
+        return {
+           ...elemClass,
+           [x]: 'valid',
+        }
+     })
+    }
+  })
+};
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(!e.target.value){
-      setErrorMessage({
-        ...errorMessage,
-        [e.target.name]: "Fild is required"
-      })
-      console.log(e.target.name.name)
-    }
-
-    setRequire(true)
     console.log("Register form data here ",formData);
+
+    Object.keys(formData).forEach(x=>{
+      if(formData[x]===""){
+        setClassName(elemClass => {
+          return {
+             ...elemClass,
+             [x]: 'invalid',
+          }
+       })
+       setErrors(y=>{
+         return {
+           ...y,
+           [x]: "Fild is require!"
+         }
+       })
+      }
+    })
   };
-    console.log("setErrorMessage ",errorMessage);
+    console.log("className ",className);
 
   return (
     <div className="registerWrapper">
@@ -43,7 +95,7 @@ export default function Register() {
 
       <form onSubmit={handleSubmit}>
         <label>Company name *</label>
-        <InputComp require={require} type="text" name="name" placeholder="Company name" message={errorMessage.name} onChange={handleChange} />
+        <InputComp className={ className?.name } type="text" name="name" placeholder="Company name" onChange={handleChange} error={errors?.name}/>
 
         <label>Country *</label>
         <div className='selectCountryWrapper'>
@@ -53,7 +105,7 @@ export default function Register() {
             <option name="Albania">Albania</option>
             <option name="Armenia">Armenia</option>
           </select>
-          <span>{require && "Please select Country!"}</span>
+          <span>{errors?.selectCountry}</span>
         </div>
         
         <label>Tel *</label>
@@ -61,18 +113,18 @@ export default function Register() {
           <select className="selectCode" name="selectCode" onChange={handleChange} unselectable="on" readOnly={true} disabled>
             <option name="SelectOption">{!formData.selectCountry ? "Select": countryPhoneCode[formData.selectCountry]}</option>
           </select>
-          <InputComp require={require} type="tel" name="tel" placeholder="Tel" message={errorMessage.tel} onChange={handleChange} />
+          <InputComp className={ className?.tel } type="tel" name="tel" placeholder="Tel" onChange={handleChange} error={errors?.tel}/>
         </div>
         
         <label>Email (Login) *</label>
-        <InputComp require={require} type="text" name="email" placeholder="Email (Login)" message={errorMessage.email} onChange={handleChange} />
+        <InputComp className={ className?.email } type="text" name="email" placeholder="Email (Login)" onChange={handleChange} error={errors?.email}/>
 
-        <label>Password *</label>        
-        <InputComp require={require} type="password" name="password" placeholder="Password" message={errorMessage.password} onChange={handleChange} />
-
-        <label>Confirm Password *</label>        
-        <InputComp require={require} type="password" name="confirmPassword" placeholder="Password" message={errorMessage.confirmPassword} onChange={handleChange} />
-
+        <label>Password *</label> 
+        <InputComp className={ className?.password } type="password" name="password" placeholder="Password" onChange={handleChange} error={errors?.password}/>
+       
+        <label>Confirm Password *</label>   
+        <InputComp className={ className?.confirmPassword } type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} error={errors?.confirmPassword}/>
+     
         <div className='agreeCheckboxWrapper'>
           <div className='agreeCheckbox'>
             <input type="checkbox" name="agreeCheckbox" onChange={handleChange}/>
@@ -82,7 +134,7 @@ export default function Register() {
               <a rel="noopener noreferrer" href="https://ubicross.com/en/store/privacy" target="_blank"> Privacy Policy</a>
             </span>
           </div>
-          <span>{require && "Please read and confirm Terms and Privacy!"}</span>
+          <span>{errors?.agreeCheckbox}</span>
         </div>
 
         {/* registration link and login button */}
