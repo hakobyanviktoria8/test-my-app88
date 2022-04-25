e062e7e7d0be1d16115346de6bcfc76e80daa746
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-25 16:40:38 +0400

    add list page digit task

--- src/pages/lists.js
+++ src/pages/lists.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import "./../styles/lists.css"
 
 export default function Lists() {
   return (
     <Layout>
         <h2>Lists page</h2>
+        <div id="circleWrapper">
+           <div id='circleBorder'>
+            <div id="squar">
+                <div id="circle1" className='circle'></div>
+                <div id="circle2" className='circle'></div>
+                <div id="circle3" className='circle'></div>
+                <div id="circle4" className='circle'></div>
+            </div> 
+          </div> 
+        </div>
     </Layout>
   );
 }
